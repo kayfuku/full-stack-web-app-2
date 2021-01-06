5c0849d095f850d378d75626d514e890c761efc8
Kei Fukutani <kifinewera@gmail.com>
2021-01-05 23:23:02 -0800

    fixed FormView.css.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,10 +11,38 @@ class Header extends Component {
   render() {
     return (
       <div className="App-header">
-        <h1 onClick={() => {this.navTo('')}}>Udacitrivia</h1>
-        <h2 onClick={() => {this.navTo('')}}>List</h2>
-        <h2 onClick={() => {this.navTo('/add')}}>Add</h2>
-        <h2 onClick={() => {this.navTo('/play')}}>Play</h2>
+        <h1
+          className="udacitrivia"
+          onClick={() => {
+            this.navTo("");
+          }}
+        >
+          Udacitrivia
+        </h1>
+        <h2
+          className="list"
+          onClick={() => {
+            this.navTo("");
+          }}
+        >
+          List
+        </h2>
+        <h2
+          className="add"
+          onClick={() => {
+            this.navTo("/add");
+          }}
+        >
+          Add
+        </h2>
+        <h2
+          className="play"
+          onClick={() => {
+            this.navTo("/play");
+          }}
+        >
+          Play
+        </h2>
       </div>
     );
   }
--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -12,7 +12,7 @@ class QuizView extends Component {
         quizCategory: null,
         previousQuestions: [], 
         showAnswer: false,
-        categories: {},
+        categories: [],
         numCorrect: 0,
         currentQuestion: {},
         guess: '',
