ea7fa9fcfc84d21d7d9dc19ac8fce9981d177e3b
Kei Fukutani <kifinewera@gmail.com>
2021-01-05 22:38:59 -0800

    added tests for get questions, categories and errors.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -50,7 +50,6 @@ def create_app(test_config=None):
     })
 
 
-
   def paginate_questions(request, selection):
     # Pagination
     page = request.args.get('page', 1, type=int)
@@ -171,6 +170,42 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False, 
+      "error": 404, 
+      "message": "resource not found"
+    }), 404
+
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False, 
+      "error": 422, 
+      "message": "unprocessable"
+    }), 422
+
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      "success": False, 
+      "error": 400, 
+      "message": "bad request"
+    }), 400
+
+
+  @app.errorhandler(405)
+  def method_not_allowed(error):
+    return jsonify({
+      "success": False, 
+      "error": 405, 
+      "message": "method not allowed"
+    }), 405
+
+
   
   return app
 
